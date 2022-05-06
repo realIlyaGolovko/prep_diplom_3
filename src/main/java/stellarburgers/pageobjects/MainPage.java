519665e7fbf27b7cb65ebf3ae5b86cc111a76df9
realIlyaGolovko <real-golovko-ilya@yandex.ru>
2022-05-06 15:33:55 +0300

    final

--- src/main/java/stellarburgers/pageobjects/MainPage.java
+++ src/main/java/stellarburgers/pageobjects/MainPage.java
@@ -6,46 +6,36 @@ import io.qameta.allure.Step;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
-import static com.codeborne.selenide.Condition.enabled;
-import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.*;
 
 public class MainPage {
     //локатор заголовка "Соберите бургер"
-    @FindBy (how = How.XPATH, using = ".//h1[text()='Соберите бургер']")
+    @FindBy(how = How.XPATH, using = ".//h1[text()='Соберите бургер']")
     private static SelenideElement headingCreateBurger;
     //локатор кнопки "Войти в аккаунт"
     @FindBy(how = How.XPATH, using = ".//button[text()='Войти в аккаунт']")
     private static SelenideElement signInButton;
+
     // Раздел «Конструктор»
     //локатор кнопки "Булки"
-    @FindBy(how = How.XPATH,using = "//span[contains(text(),'Булки')]")
+    @FindBy(how = How.XPATH, using = "//span[contains(text(),'Булки')]")
     private static SelenideElement bunButton;
     //локатор кнопки "Соусы"
-    @FindBy(how = How.XPATH,using = "//span[contains(text(),'Соусы')]")
+    @FindBy(how = How.XPATH, using = "//span[contains(text(),'Соусы')]")
     private static SelenideElement saucesButton;
     //локатор кнопки "Начинки"
-    @FindBy(how = How.XPATH,using = "//span[contains(text(),'Начинки')]")
+    @FindBy(how = How.XPATH, using = "//span[contains(text(),'Начинки')]")
     private static SelenideElement fillingButton;
-
-    //локатор раздел «Булки»
-    @FindBy(how = How.XPATH, using = ".//h2[text()='Булки']")
-    private static SelenideElement bunSection;
-    //локатор раздел «Соусы»
-    @FindBy(how = How.XPATH, using = ".//h2[text()='Соусы']")
-    private static SelenideElement sauceSection;
-    //локатор раздел «Начинки»
-    @FindBy(how = How.XPATH, using = ".//h2[text()='Начинки']")
-    private static SelenideElement fillingSection;
-
     //локатор активного раздела
-    @FindBy(how = How.CSS,using = ".tab_tab_type_current__2BEPc")
+    @FindBy(how = How.CSS, using = ".tab_tab_type_current__2BEPc")
     public SelenideElement activeSection;
 
     @Step("Получение текста заголовка 'Соберите бургер'")
-    public String getHeadingCreateBurger(){
+    public String getHeadingCreateBurger() {
         headingCreateBurger.shouldBe(Condition.visible);
         return headingCreateBurger.getText();
     }
+
     @Step("Клик по кнопке 'Войти в аккаунт'")
     public void clickSignInButton() {
         signInButton.scrollTo();
@@ -54,12 +44,12 @@ public class MainPage {
 
     // Раздел «Конструктор»
     @Step("Клик по разделу «Булки»")
-    public  void clickBunButton() {
+    public void clickBunButton() {
         bunButton.shouldBe(enabled).click();
     }
 
     @Step("Клик по разделу «Соусы»")
-    public  void clickSauceButton() {
+    public void clickSauceButton() {
         saucesButton.click();
     }
 
@@ -68,22 +58,8 @@ public class MainPage {
         fillingButton.shouldBe(enabled).click();
     }
 
-    @Step("Проверка корректности отображения раздела Булки")
-    public void checkBunSectionText() {
-        bunSection.shouldHave(exactText("Булки"));
-    }
-
-    @Step("Проверка корректности отображения раздела Соусы")
-    public void checkSaucesSectionText() {
-        sauceSection.shouldHave(exactText("Соусы"));
-    }
-
-    @Step("Проверка корректности отображения раздела Начинки")
-    public void checkFillingSectionText() {
-        fillingSection.shouldHave(exactText("Начинки"));
-    }
     @Step("Получение текста активного раздела")
-    public String getActiveSectionText(){
-         return activeSection.getText();
+    public String getActiveSectionText() {
+        return activeSection.getText();
     }
 }
--- src/test/java/stellarburgers/ConstructorTest.java
+++ src/test/java/stellarburgers/ConstructorTest.java
@@ -4,32 +4,46 @@ import io.qameta.allure.junit4.DisplayName;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import stellarburgers.common.CommonTest;
 import stellarburgers.pageobjects.MainPage;
 
 import static com.codeborne.selenide.Selenide.open;
 import static stellarburgers.pageobjects.PageConstants.MAIN_PAGE_URL;
 
-public class ConstructorTest {
+public class ConstructorTest extends CommonTest {
     private static MainPage mainPage;
+
     @Before
-    public void openManePage(){
-       mainPage=open(MAIN_PAGE_URL,MainPage.class);
+    public void openManePage() {
+        mainPage = open(MAIN_PAGE_URL, MainPage.class);
     }
+
     @Test
-    @DisplayName("При открытии главной страницы отображается рздел 'Булки'")
-    public void manePageDefaultOpenBunSection(){
+    @DisplayName("При открытии главной страницы отображается раздел 'Булки'")
+    public void manePageDefaultOpenBunSection() {
+        Assert.assertEquals("Булки", mainPage.getActiveSectionText());
     }
+
     @Test
     @DisplayName("Переход к разделу 'Соусы'")
-    public void transitionToSaucesSection(){
+    public void transitionToSaucesSection() {
+        mainPage.clickSauceButton();
+        Assert.assertEquals("Соусы", mainPage.getActiveSectionText());
     }
+
     @Test
     @DisplayName("Переход к разделу 'Начинки'")
-    public void transitionToFillingSection(){
+    public void transitionToFillingSection() {
+        mainPage.clickFillingButton();
+        Assert.assertEquals("Начинки", mainPage.getActiveSectionText());
     }
+
     @Test
     @DisplayName("Переход к разделу 'Булки'")
-    public void transitionToBunSection(){
+    public void transitionToBunSection() {
+        mainPage.clickFillingButton();
+        mainPage.clickBunButton();
+        Assert.assertEquals("Булки", mainPage.getActiveSectionText());
     }
 
 }
